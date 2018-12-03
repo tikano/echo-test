64caeefb68766f4d33d1a463496b9a1689192bd7
tikano <oadmathgenie@gmail.com>
2018-12-02 21:21:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ restService.post("/echo", function(req, res) {
       ? req.body.queryResult.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: speech,
+    speech: "om",
     displayText: speech,
     source: "webhook-echo-sample"
   });
