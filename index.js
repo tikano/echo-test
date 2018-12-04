a6221b0f492b4eb25049f458b4d5417516d4b044
tikano <oadmathgenie@gmail.com>
2018-12-03 21:37:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 
 const restService = express();
 
+class Assignment{
+  
+  
+}
+
 restService.use(
   bodyParser.urlencoded({
     extended: true
@@ -13,12 +18,35 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
+restService.post("/hw", function(req, res) {
+  var type1 = req.body.queryResult.parameters.type1;
+  var type2 = req.body.queryResult.parameters.type2;
+  var type3 = req.body.queryResult.parameters.type3;
+  var type1 = req.body.queryResult.parameters.type1;
+  var subject1 = req.body.queryResult.parameters.subject1;
+  var subject2 = req.body.queryResult.parameters.subject2;
+  var subject3 = req.body.queryResult.parameters.subject3;
+  var date1 = req.body.queryResult.parameters.date1;
+  var date2 = req.body.queryResult.parameters.date2;
+  var date3 = req.body.queryResult.parameters.date3;
   var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.type1 
       ? req.body.queryResult.parameters.type1
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    fulfillmentText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.firstName 
+      ? req.body.queryResult.parameters.firstName + " " + req.body.queryResult.parameters.lastName
       : "Seems like some problem. Speak again.";
   return res.json({
     fulfillmentText: speech,
